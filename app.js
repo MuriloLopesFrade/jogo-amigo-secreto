5693d5a00b892036e956890ce7ed91a65d62977a
MuriloLopesFrade <murilo.l.frade@gmail.com>
2025-01-29 16:44:57 -0300

    Código do projeto

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Array para armazenar os nomes
+let amigos = [];
 
-let nomeParaSorteio = [];
+//Função de adicionar os nomes dos amigos na lista 
+function adicionarAmigo() {
+    let entrada = document.querySelector('input').value;
 
+    if (entrada == '') {
+        alert('Por Favor, insira um nome!');
+    } else {
+        amigos.push(entrada);
+        document.querySelector('input').value = '';
+    }
+
+}
+
+// Função usada para atualizar a lista de amigos
+function atualizarAmigo() {
+
+    for (let amigo in amigos) {
+        let novoItem = document.createElement("li");
+        novoItem.innerHTML = amigo;
+        amigos.push(novoItem);
+    }
+}
+
+// Função usada para sortiar um nome da lista de amigos
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        alert('Necesario adicionar os nomes ante de sortear!')
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length)
+        let amigoSorteado = amigos[indiceAleatorio];
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `Amigo sortiado foi ${amigoSorteado}`
+    }
+
+}
